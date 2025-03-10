e4ebdc630f5939edfbcd0ddc991a5c4b7d65ab3d
YOGEESH K P <yogeesh793@gmail.com>
2025-03-10 10:02:05 +0530

    Add files via upload

---
+++ Array_Indexing_6.py
@@ -0,0 +1,55 @@
+# Access the third element of the second array of the first array:
+import numpy as np
+arr = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr[0,1,2])
+
+#1
+import numpy as ap
+app=ap.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(app[0,1,2])
+
+#2
+import numpy as bp
+bpp=bp.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(bpp[0,1,2])
+
+#3
+import numpy as cp
+cpp=cp.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(cpp[0,1,2])
+
+#4
+import numpy as dp
+dpp=dp.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(dpp[0,1,2])
+
+#5
+import numpy as ep
+epp=ep.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(epp[0,1,2])
+
+#6
+import numpy as fp
+fpp=fp.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(fpp[0,1,2])
+
+#7
+import numpy as gp
+gpp=gp.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(gpp[0,1,2])
+
+#8
+import numpy as hp
+hpp=hp.array([[[1,2,3],[4,5,7]],[[7,8,9],[10,11,12]]])
+print(hpp[0,1,2])
+
+#9
+import numpy as ip
+ipp=ip.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(ipp[0,1,2])
+
+#10
+import numpy as jp
+jpp=jp.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(jpp[0,1,2])
+
